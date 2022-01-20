83d34f2a06b7970a66a1289b85b8fc728facdea5
AchuthKrishna <92132574+AchuthKrishna@users.noreply.github.com>
2022-01-19 16:26:13 -0800

    Update MarkdownParse.java
    Fixed the infinite loop issue from the  markdown file with a new line at the end.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,11 +11,15 @@ public class MarkdownParse {
         // the next )
         int currentIndex = 0;
         System.out.println(currentIndex);
-        while(currentIndex < markdown.length()) {
+        boolean foundParen = true;
+        while(currentIndex < markdown.length() && foundParen) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            if(!markdown.substring(closeParen+1, markdown.length()).contains(")")){
+                foundParen = false;
+            }
             if(markdown.substring(openParen+1, closeParen).contains(".") &&
                !markdown.substring(openParen+1, closeParen).contains(" ")){
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
