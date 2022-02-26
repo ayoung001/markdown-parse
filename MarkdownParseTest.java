536dcec2e45bc8d80ba8e4af25bd1cf6e39f740d
ayoung001 <asy001@ucsd.edu>
2022-02-25 16:38:17 -0800

    for lab report 4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -19,4 +19,28 @@ public class MarkdownParseTest {
         List<String> check = List.of("https://something.com", "some-page.html");
         assertEquals(links, check);
     }
+    
+    @Test
+    public void testSnippet1() throws IOException
+    {
+      Path fileName = Path.of("snippet1.md");
+      String contents = Files.readString(fileName);
+      assertEquals(List.of("`google.com","google.com","ucsd.edu"),MarkdownParse.getLinks(contents));
+    }
+  
+    @Test
+    public void testSnippet2() throws IOException
+    {
+      Path fileName = Path.of("snippet2.md");
+      String contents = Files.readString(fileName);
+      assertEquals(List.of("a.com","a.com(())","example.com"),MarkdownParse.getLinks(contents));
+    }
+  
+    @Test
+    public void testSnippet3() throws IOException
+    {
+      Path fileName = Path.of("snippet3.md");
+      String contents = Files.readString(fileName);
+      assertEquals(List.of("https://www.twitter.com ","https://ucsd-cse15l-w22.github.io/","https://cse.ucsd.edu"),MarkdownParse.getLinks(contents));
+    }
 }
